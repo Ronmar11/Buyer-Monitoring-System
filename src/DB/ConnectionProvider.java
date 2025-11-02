48b894bc6e65eecae2ea2ef73d28da13065977a2
Ronmar11 <109712770+Ronmar11@users.noreply.github.com>
2025-11-02 23:06:55 +0800

    Iniatial Commit

---
+++ src/DB/ConnectionProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DB;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Ronmar abalos
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+    
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localHost:3306");
+            
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+}
---
+++ src/Information/User.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Information;
+
+/**
+ *
+ * @author Ronmar abalos
+ */
+public class User {
+    private String fName;
+    private String mName;
+    private String lName;
+    private String email;
+    private String gender;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+}
